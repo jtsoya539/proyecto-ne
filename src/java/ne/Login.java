020b9d439154364148b188657c1cc49ee68a6e6c
DamyGenius <damian.meza.py@hotmail.com>
2018-06-02 14:46:43 -0400

    Cambio en servlet de Login
    Mejora en el tipo de comunicacion del Login

--- src/java/ne/Login.java
+++ src/java/ne/Login.java
@@ -3,6 +3,8 @@ package ne;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,12 +13,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @WebServlet(name = "Login", urlPatterns = {"/Login"})
 public class Login extends HttpServlet {
 
     BaseDatos db;
     int errorCode;
+    String state;
     String message;
 
     public Login() {
@@ -66,79 +71,85 @@ public class Login extends HttpServlet {
 
         String responsePage;
         this.errorCode = 0;
+        this.state = null;
         this.message = null;
 
         // Obtener parametros
         String usuario = request.getParameter("usuario");
         String clave = request.getParameter("clavemd5");
 
+        String resultado = null;
         String mensaje = null;
         String menu = null;
         //String seleccionJugadores = null;
 
         try {
             db.conectar("ne", "ruffus");
-            mensaje = db.ejecutarFuncionString("f_valida_usuario('" + usuario + "', '" + clave + "')");
+            resultado = db.ejecutarFuncionString("f_valida_usuario('" + usuario + "', '" + clave + "')");
+
+            try {
+                //Parsear la respuesta
+                JSONObject json1 = new JSONObject(resultado);
+                state = json1.getString("state");
+                message = json1.getString("message");
+            } catch (JSONException ex) {
+                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            mensaje = db.ejecutarFuncionString("k_aplicacion_web.f_mensaje('" + message + "')");
             db.cerrar();
         } catch (SQLException ex) {
             errorCode = ex.getErrorCode();
-            message = ex.getMessage();
+            resultado = ex.getMessage();
+            System.out.println("SQLException: " + errorCode + ' ' + resultado);
         }
 
-        if (errorCode != 0) {
-            try {
-                db.conectar("ne", "ruffus");
-                mensaje = db.ejecutarFuncionString("k_aplicacion_web.f_mensaje('" + db.limpiarMensajeError(message) + "')");
-                db.cerrar();
-            } catch (SQLException ex) {
-                errorCode = ex.getErrorCode();
-                message = ex.getMessage();
-                System.out.println("SQLException: " + errorCode + ' ' + message);
-            }
+        if (errorCode == 0) { //si no hubo error en la validacion del usuario
 
-        } else {
+            if(state.equals("OK")) { //si las credenciales son correctas
 
-            try {
-                db.conectar("ne", "ruffus");
-                db.ejecutarProcedimiento("k_sistema.p_set_usuario('" + usuario + "')");
-                menu = db.ejecutarFuncionClob("k_aplicacion_web.f_menu('WEB')");
-                //seleccionJugadores = db.ejecutarFuncionClob("k_aplicacion_web.f_seleccion_jugadores('WEB')");
-                db.cerrar();
-            } catch (SQLException ex) {
-                errorCode = ex.getErrorCode();
-                message = ex.getMessage();
-            }
-
-            if (errorCode != 0) {
                 try {
                     db.conectar("ne", "ruffus");
-                    mensaje = db.ejecutarFuncionString("k_aplicacion_web.f_mensaje('" + db.limpiarMensajeError(message) + "')");
+                    db.ejecutarProcedimiento("k_sistema.p_set_usuario('" + usuario + "')");
+                    menu = db.ejecutarFuncionClob("k_aplicacion_web.f_menu('WEB')");
+                    //seleccionJugadores = db.ejecutarFuncionClob("k_aplicacion_web.f_seleccion_jugadores('WEB')");
                     db.cerrar();
                 } catch (SQLException ex) {
                     errorCode = ex.getErrorCode();
                     message = ex.getMessage();
-                    System.out.println("SQLException: " + errorCode + ' ' + message);
                 }
 
-            } else {
-                // Agregar sesion
-                HttpSession sesion = request.getSession();
-                sesion.setAttribute("usuario", usuario);
-                sesion.setAttribute("clave", clave);
-                sesion.setMaxInactiveInterval(60 * 15); // 15 minutos
+                if (errorCode != 0) {
+                    try {
+                        db.conectar("ne", "ruffus");
+                        mensaje = db.ejecutarFuncionString("k_aplicacion_web.f_mensaje('" + db.limpiarMensajeError(message) + "')");
+                        db.cerrar();
+                    } catch (SQLException ex) {
+                        errorCode = ex.getErrorCode();
+                        message = ex.getMessage();
+                        System.out.println("SQLException: " + errorCode + ' ' + message);
+                    }
 
-                // Agregar cookie
-                Cookie cookie = new Cookie("usuario", usuario);
-                cookie.setPath("/ProyectoNE");
-                cookie.setMaxAge(60 * 60); // 60 minutos
-                response.addCookie(cookie);
+                } else {
+                    // Agregar sesion
+                    HttpSession sesion = request.getSession();
+                    sesion.setAttribute("usuario", usuario);
+                    sesion.setAttribute("clave", clave);
+                    sesion.setMaxInactiveInterval(60 * 15); // 15 minutos
+
+                    // Agregar cookie
+                    Cookie cookie = new Cookie("usuario", usuario);
+                    cookie.setPath("/ProyectoNE");
+                    cookie.setMaxAge(60 * 60); // 60 minutos
+                    response.addCookie(cookie);
+                }
 
             }
 
         }
 
         try (PrintWriter out = response.getWriter()) {
-            if (errorCode != 0) {
+            if (errorCode != 0 || !state.equals("OK")) {
                 responsePage = "index.html";
             } else {
                 responsePage = "base.html";
@@ -151,9 +162,6 @@ public class Login extends HttpServlet {
             if (menu != null) {
                 out.println(menu);
             }
-            /*if (seleccionJugadores != null) {
-                out.println(seleccionJugadores);
-            }*/
             out.close();
         }
 
